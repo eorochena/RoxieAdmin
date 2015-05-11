257de4a32c81b376cea16618eb72991038b3be86
Eduardo Orochena <mint.alchemist@gmail.com>
2015-05-11 09:56:21 -0400

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,5 @@
+cp Bash-Complete/Moxie /etc/bash_completion.d/
+cp Bash-Complete/auto.sh /etc/profile.d/
+cp Executables/Moxie /usr/bin/
+cp Executables/Moo* /usr/games/
+cp ManPages/moxie.8.gz /usr/share/man/man8/
